70a26de4bf114038d6cfd3d9b90cf069d2dd6d61
fxxxysh <1208207637@qq.com>
2018-05-02 18:13:17 +0800

    no message

--- comlink/comlink.h
+++ comlink/comlink.h
@@ -5,14 +5,12 @@
 #ifdef DLL_EXPORTS
 #define DLL_API __declspec(dllexport)
 #else
-#define DLL_API __declspec(dllimport)
+//#define DLL_API __declspec(dllimport)
+#define DLL_API extern
 #endif
 
 #include "stdint.h"
 
-DLL_API void comlink_test(void);
+DLL_API uint8_t comlink_parse(uint8_t *buffer, int buffer_size, uint8_t* msg);
 
-DLL_API bool comlink_parse(uint8_t *buffer, int buffer_size);
-
-DLL_API bool comlink_get_msg(uint8_t* msg_number, uint8_t* now_msg,
-	uint16_t* len, uint8_t* payload, uint8_t* seq, uint8_t* sysid, uint8_t* compid, uint8_t* msgid);
+DLL_API uint8_t comlink_encode(uint8_t* msg, const uint8_t *packet, uint8_t compid, uint8_t msg_id, uint8_t length);
--- toolkit/modules/comlink/comlink.cs
+++ toolkit/modules/comlink/comlink.cs
@@ -8,19 +8,48 @@ using Iocomp.Instrumentation.Plotting;
 using System.Runtime.InteropServices;
 using toolkit;
 
+
 namespace toolkit.modules
 {
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct __mavlink_attitude_t
+    {
+        public byte tt;
+        public UInt32 time_boot_ms; /*< Timestamp (milliseconds since system boot)*/
+        public float roll; /*< Roll angle (rad, -pi..+pi)*/
+        public float pitch; /*< Pitch angle (rad, -pi..+pi)*/
+        public float yaw; /*< Yaw angle (rad, -pi..+pi)*/
+        public float rollspeed; /*< Roll angular speed (rad/s)*/
+        public float pitchspeed; /*< Pitch angular speed (rad/s)*/
+        public float yawspeed; /*< Yaw angular speed (rad/s)*/
+    };
+
+    struct message_t
+    {
+        public byte magic; //帧头
+        public byte len; //负载长度
+        public byte seq; //序列码
+                         //uint8_t flags; //标志位
+        public byte sysid; //系统ID
+        public byte compid; //组件ID
+        public byte msgid; //消息ID
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 258, ArraySubType = UnmanagedType.U1)]
+        public byte[] payload;
+
+        UInt16 checksum; //实时校验
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2, ArraySubType = UnmanagedType.U1)]
+        public byte[] ck; //接收校验
+    };
+
     public partial class s_comlink
     {
-        [DllImport("../../../Debug/comlink.dll", EntryPoint = "comlink_test", CallingConvention = CallingConvention.Cdecl)]
-        extern static void comlink_test();
-
         [DllImport("../../../Debug/comlink.dll", EntryPoint = "comlink_parse", CallingConvention = CallingConvention.Cdecl)]
-        extern static bool comlink_parse(byte[] buffer, int buffer_size);
+        extern static byte comlink_parse(ref byte buffer, int buffer_size, byte[] msg);
 
-        [DllImport("../../../Debug/comlink.dll", EntryPoint = "comlink_get_msg", CallingConvention = CallingConvention.Cdecl)]
-        extern static bool comlink_get_msg(byte[] msg_number, byte[] now_msg,
-            UInt16[] len, byte[] payload, byte[] seq, byte[] sysid, byte[] compid, byte[] msgid);
+        [DllImport("../../../Debug/comlink.dll", EntryPoint = "comlink_encode", CallingConvention = CallingConvention.Cdecl)]
+        extern static byte comlink_encode(ref byte msg, byte[] packet, byte compid, byte msg_id, byte length);
 
         public s_comlink()
         {
@@ -29,11 +58,38 @@ namespace toolkit.modules
 
         public bool parse(byte[] buffer, int size)
         {
-            //byte[] buff123 = new byte[10];
+            byte[] buff123 = new byte[10];
 
-            bool m = comlink_parse(buffer, size);
+            __mavlink_attitude_t mavlink_attitude;
+            message_t[] receive_msg = new message_t[64];
 
-            comlink_test();
+            mavlink_attitude.pitch = 89.12f;
+            mavlink_attitude.pitchspeed = 0.12f;
+            mavlink_attitude.roll = 178.23f;
+            mavlink_attitude.rollspeed = 1.21f;
+            mavlink_attitude.time_boot_ms = 12345678;
+            mavlink_attitude.yaw = 272.78f;
+            mavlink_attitude.yawspeed = 2.65f;
+
+            string arr = "23r";
+            //fixed (int* p = arr) ;
+
+                byte pkg_length = (byte)Marshal.SizeOf(typeof(__mavlink_attitude_t));
+
+            Byte[] bPara = new Byte[100];    //新建字节数组 
+            comlink_encode(ref bPara[0], (byte[])buff123, 1, 2, pkg_length);
+
+           // byte m = comlink_parse(buffer, size, buff123);
+
+            //comlink_test();
+
+            //_comlink->packet_send
+
+            //int msg_length = HEADER_LEN + pkg_length + 3;
+            //if (_comlink->parse_char((uint8_t*)&test_trans_msg, msg_length) == FRAMING_OK)
+            //{
+            //    memcpy(&rx_mavlink_attitude, _MSG_PAYLOAD(&test_trans_msg), pkg_length);
+            //}
 
             return true;
         }
