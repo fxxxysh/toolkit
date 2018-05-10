d3fa884ea99099ea0d3caf8d54a4aa9ce2af3c26
fxxxysh <1208207637@qq.com>
2018-05-10 17:03:31 +0800

    no message

--- dev_toolkit/dev_toolkit.Designer.cs
+++ dev_toolkit/dev_toolkit.Designer.cs
@@ -44,13 +44,21 @@
             this.navigationFrame1 = new DevExpress.XtraBars.Navigation.NavigationFrame();
             this.navigationPage1 = new DevExpress.XtraBars.Navigation.NavigationPage();
             this.navigationPage2 = new DevExpress.XtraBars.Navigation.NavigationPage();
-            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.kit_hide = new DevExpress.XtraBars.BarButtonItem();
+            this.kit_com_connect_dis = new DevExpress.XtraBars.BarButtonItem();
+            this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.kit_sys_id = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemSpinEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemSpinEdit();
+            this.kit_dev_id = new DevExpress.XtraBars.BarEditItem();
+            this.repositoryItemSpinEdit2 = new DevExpress.XtraEditors.Repository.RepositoryItemSpinEdit();
             ((System.ComponentModel.ISupportInitialize)(this.kit_ribbon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemComboBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemComboBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.navBarControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.navigationFrame1)).BeginInit();
             this.navigationFrame1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemSpinEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemSpinEdit2)).BeginInit();
             this.SuspendLayout();
             // 
             // kit_ribbon
@@ -61,16 +69,22 @@
             this.kit_com_port,
             this.kit_com_baudrate,
             this.kit_com_connect,
-            this.barButtonItem1});
+            this.kit_hide,
+            this.kit_com_connect_dis,
+            this.kit_sys_id,
+            this.kit_dev_id});
             this.kit_ribbon.Location = new System.Drawing.Point(0, 0);
-            this.kit_ribbon.MaxItemId = 5;
+            this.kit_ribbon.MaxItemId = 8;
             this.kit_ribbon.Name = "kit_ribbon";
             this.kit_ribbon.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.kit_ribbon.QuickToolbarItemLinks.Add(this.barButtonItem1);
+            this.kit_ribbon.QuickToolbarItemLinks.Add(this.kit_hide);
+            this.kit_ribbon.QuickToolbarItemLinks.Add(this.kit_com_connect_dis);
             this.kit_ribbon.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
             this.repositoryItemComboBox1,
-            this.repositoryItemComboBox2});
+            this.repositoryItemComboBox2,
+            this.repositoryItemSpinEdit1,
+            this.repositoryItemSpinEdit2});
             this.kit_ribbon.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
             this.kit_ribbon.ShowToolbarCustomizeItem = false;
             this.kit_ribbon.Size = new System.Drawing.Size(1022, 150);
@@ -117,7 +131,8 @@
             // 
             this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
             this.ribbonPageGroup1,
-            this.ribbonPageGroup2});
+            this.ribbonPageGroup2,
+            this.ribbonPageGroup3});
             this.ribbonPage1.Name = "ribbonPage1";
             this.ribbonPage1.Text = "连接";
             // 
@@ -181,13 +196,58 @@
             this.navigationPage2.Name = "navigationPage2";
             this.navigationPage2.Size = new System.Drawing.Size(853, 449);
             // 
-            // barButtonItem1
+            // kit_hide
             // 
-            this.barButtonItem1.Caption = "kit_hide";
-            this.barButtonItem1.Id = 4;
-            this.barButtonItem1.ImageOptions.Image = global::dev_toolkit.Properties.Resources.group2_32x32;
-            this.barButtonItem1.ImageOptions.LargeImage = global::dev_toolkit.Properties.Resources.group2_32x32;
-            this.barButtonItem1.Name = "barButtonItem1";
+            this.kit_hide.Caption = "kit_hide";
+            this.kit_hide.Id = 4;
+            this.kit_hide.ImageOptions.Image = global::dev_toolkit.Properties.Resources.group2_32x32;
+            this.kit_hide.ImageOptions.LargeImage = global::dev_toolkit.Properties.Resources.group2_32x32;
+            this.kit_hide.Name = "kit_hide";
+            // 
+            // kit_com_connect_dis
+            // 
+            this.kit_com_connect_dis.Caption = "barButtonItem1";
+            this.kit_com_connect_dis.Id = 5;
+            this.kit_com_connect_dis.ImageOptions.Image = global::dev_toolkit.Properties.Resources.play_16x16;
+            this.kit_com_connect_dis.ImageOptions.LargeImage = global::dev_toolkit.Properties.Resources.stop_32x32;
+            this.kit_com_connect_dis.Name = "kit_com_connect_dis";
+            // 
+            // ribbonPageGroup3
+            // 
+            this.ribbonPageGroup3.ItemLinks.Add(this.kit_sys_id);
+            this.ribbonPageGroup3.ItemLinks.Add(this.kit_dev_id);
+            this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+            this.ribbonPageGroup3.Text = "ID配置";
+            // 
+            // kit_sys_id
+            // 
+            this.kit_sys_id.Caption = "系统ID";
+            this.kit_sys_id.Edit = this.repositoryItemSpinEdit1;
+            this.kit_sys_id.EditWidth = 80;
+            this.kit_sys_id.Id = 6;
+            this.kit_sys_id.Name = "kit_sys_id";
+            // 
+            // repositoryItemSpinEdit1
+            // 
+            this.repositoryItemSpinEdit1.AutoHeight = false;
+            this.repositoryItemSpinEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemSpinEdit1.Name = "repositoryItemSpinEdit1";
+            // 
+            // kit_dev_id
+            // 
+            this.kit_dev_id.Caption = "设备ID";
+            this.kit_dev_id.Edit = this.repositoryItemSpinEdit2;
+            this.kit_dev_id.EditWidth = 80;
+            this.kit_dev_id.Id = 7;
+            this.kit_dev_id.Name = "kit_dev_id";
+            // 
+            // repositoryItemSpinEdit2
+            // 
+            this.repositoryItemSpinEdit2.AutoHeight = false;
+            this.repositoryItemSpinEdit2.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemSpinEdit2.Name = "repositoryItemSpinEdit2";
             // 
             // dev_toolkit
             // 
@@ -206,6 +266,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.navBarControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.navigationFrame1)).EndInit();
             this.navigationFrame1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemSpinEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemSpinEdit2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +290,12 @@
         private DevExpress.XtraEditors.Repository.RepositoryItemComboBox repositoryItemComboBox2;
         private DevExpress.XtraBars.BarButtonItem kit_com_connect;
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
-        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem kit_hide;
+        private DevExpress.XtraBars.BarButtonItem kit_com_connect_dis;
+        private DevExpress.XtraBars.BarEditItem kit_sys_id;
+        private DevExpress.XtraEditors.Repository.RepositoryItemSpinEdit repositoryItemSpinEdit1;
+        private DevExpress.XtraBars.BarEditItem kit_dev_id;
+        private DevExpress.XtraEditors.Repository.RepositoryItemSpinEdit repositoryItemSpinEdit2;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
     }
 }
