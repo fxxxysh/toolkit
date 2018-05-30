2b38ba56e1ee10a7fecf086b616aee965067c28f
fxxxysh <1208207637@qq.com>
2018-05-30 18:24:34 +0800

    no message

---
+++ dev_toolkit/device/gnss/gnss_main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_toolkit.device
+{
+    public class gnss_main
+    {
+        public gnss_main()
+        {
+
+        }
+    }
+}
---
+++ dev_toolkit/device/gnss/gyro_calib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_toolkit.device
+{
+    public class gyro_calib
+    {
+        public gyro_calib()
+        {
+
+        }
+    }
+}
